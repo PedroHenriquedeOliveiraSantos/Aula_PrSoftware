3cd6c687a836d2fc40bb0c34cf5d0ab6b24fe0b2
Nícolas Ferri <nicolasbirche@gmail.com>
2025-02-12 10:40:29 -0300

    Teste1

---
+++ Aula 01 - Conceitos Básicos/src/Helloworld/helloworld.java
@@ -0,0 +1,10 @@
+package Helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.print("Essa desgraça de joça de porcaria de linguagem de merda de programação");
+
+	}
+
+}
